3d4cc9168a921590f4f175fedb9f1c779d286aad
Samuel Diogo <samuka.o.diogo@gmail.com>
2021-02-23 00:13:34 -0300

    ajustar colisoes e adicionar outros elementos

--- js/js.js
+++ js/js.js
@@ -10,10 +10,13 @@ function start() { // Inicio da função start()
 
 //Principais variáveis do jogo
 var jogo = {}
+var velocidade=5;
+var podeAtirar=true;
+var posicaoY = parseInt(Math.random() * 334);
 var TECLA = {
 	UP: 38,
 	DN: 40,
-	S: 8
+	D: 68
 	}
 
 	jogo.pressionou = [];
@@ -39,10 +42,12 @@ function loop() {
 
 movefundo();
 movejogador();
+moveamigo();
+colisao();
+moveinimigo1();
+moveinimigo2();
 
 } // Fim da função loop()
-
-
 
 //Função que movimenta o fundo do jogo
 
@@ -78,11 +83,122 @@ function movejogador() {
 		}
 	}
 	
-	if (jogo.pressionou[TECLA.S]) {
+	if (jogo.pressionou[TECLA.D]) {
 		
-		//Chama função Disparo	
+		disparo();//Chama função Disparo	
 	}
 
 	} // fim da função movejogador()
 
-}
+
+	function moveamigo() {
+	
+		posicaoX = parseInt($("#amigo").css("left"));
+		$("#amigo").css("left",posicaoX+1);
+					
+			if (posicaoX>906) {
+				
+			$("#amigo").css("left",0);
+						
+			}
+	
+	} // fim da função moveamigo()
+
+	function disparo() {
+	
+		if (podeAtirar==true) {
+			
+		podeAtirar=false;
+		
+		topo = parseInt($("#jogador").css("top"))
+		posicaoX= parseInt($("#jogador").css("left"))
+		tiroX = posicaoX + 190;
+		topoTiro=topo+52;
+		$("#fundoGame").append("<div id='disparo'></div");
+		$("#disparo").css("top",topoTiro);
+		$("#disparo").css("left",tiroX);
+		
+		var tempoDisparo=window.setInterval(executaDisparo, 20);
+		
+		} //Fecha podeAtirar
+		function executaDisparo() {
+			posicaoX = parseInt($("#disparo").css("left"));
+			$("#disparo").css("left",posicaoX+15); 
+	
+					if (posicaoX>900) {
+							
+				window.clearInterval(tempoDisparo);
+				tempoDisparo=null;
+				$("#disparo").remove();
+				podeAtirar=true;
+						
+					   }
+		}// Fecha executaDisparo()
+	} // Fecha disparo()
+
+
+	function colisao() {
+var colisao1 = ($("#jogador").collision($("#inimigo1")));
+// jogador com o inimigo1
+	
+	if (colisao1.length>0) {
+		
+	inimigo1X = parseInt($("#inimigo1").css("left"));
+	inimigo1Y = parseInt($("#inimigo1").css("top"));
+	explosao1(inimigo1X,inimigo1Y);
+
+	posicaoY = parseInt(Math.random() * 338);
+	$("#inimigo1").css("left",694);
+	$("#inimigo1").css("top",posicaoY);
+	}
+
+} //Fim da função colisao()
+
+//Explosão 1
+function explosao1(inimigo1X,inimigo1Y) {
+	$("#fundoGame").append("<div id='explosao1'></div");
+	$("#explosao1").css("background-image", "url(imgs/explosao.png)");
+	var div=$("#explosao1");
+	div.css("top", inimigo1Y);
+	div.css("left", inimigo1X);
+	div.animate({width:200, opacity:0}, "slow");
+	
+	var tempoExplosao=window.setInterval(removeExplosao, 2000);
+	
+		function removeExplosao() {
+			
+			div.remove();
+			window.clearInterval(tempoExplosao);
+			tempoExplosao=null;
+			
+		}
+		
+	} // Fim da função explosao1()
+
+	function moveinimigo1() {
+
+		posicaoX = parseInt($("#inimigo1").css("left"));
+		$("#inimigo1").css("left",posicaoX-velocidade);
+		$("#inimigo1").css("top",posicaoY);
+			
+			if (posicaoX<=0) {
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",694);
+			$("#inimigo1").css("top",posicaoY);
+				
+			}
+	} //Fim da função moveinimigo1()
+
+	function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+	$("#inimigo2").css("left",posicaoX-4);
+				
+		if (posicaoX<=0) {
+			
+		$("#inimigo2").css("left",775);
+					
+		}
+} // Fim da função moveinimigo2()
+
+
+} // fim da funçao start
